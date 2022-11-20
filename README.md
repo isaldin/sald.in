8b79ee15c2826e329969baff4245b4079b97df43
saldin <saldin.iliya@gmail.com>
2022-11-20 15:43:41 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Distribution
+Just host content of `src` folder somewhere.
+
+### Development
+Run `yarn install` and `yarn dev` then.
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html'],
+  theme: {
+    colors: {
+      white: '#f9f9f9',
+      blue: '#414766',
+      orange: '#c8782e',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
